015640b92eddf1611f9be58d71afe021d5f9e8d7
Ekin Oral <oralekin@sabanciuniv.edu>
2025-03-22 11:30:04 +0300

    add script to test all inputs

---
+++ template/test-all.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+xx=$(tput setaf 1)$(tput bold)"XX"$(tput sgr0)
+ok=$(tput setaf 2)$(tput bold)"OK"$(tput sgr0)
+all_ok=true;
+
+if ! make $1; then
+  echo -e $xx" failed to build";
+  exit;
+fi
+
+for input in ./samples/*.in; do
+  output="$(basename input).out"
+  # if theres no matching output, just check the return is 0 and show ok
+  if [ ! -f "$output" ]; then
+    if $1 < $input > /dev/null; then
+      echo -en $ok;
+    else
+      echo -en $xx;
+    fi
+    echo ": sample '"$(basename input)"'";
+  else
+    dc=$("$1" < $input | diff -U 0 $output - | grep ^@ | wc -l)
+    if [ "$dc" -eq "0" ]; then
+      echo -e $ok": sample '"$(basename input)"'";
+    else
+      echo -e $xx": sample '"$(basename input)"'";
+      echo -e "\t$dc mismatched lines";
+    fi
+  fi
+done
+
+if [ all_ok ]; then
+  echo -e $(tput setaf 2)$(tput bold)"ALL OK"$(tput sgr0);
+fi
