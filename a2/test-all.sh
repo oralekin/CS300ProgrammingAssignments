bbbbbd2d66e4c5aceabf30a7e7fe1647569fc175
Ekin Oral <oralekin@sabanciuniv.edu>
2025-03-27 09:32:31 +0300

    update testing scripts

--- a2/test-all.sh
+++ a2/test-all.sh
@@ -10,22 +10,24 @@ if ! make $1; then
 fi
 
 for input in ./samples/*.in; do
-  output="$(basename input).out"
+  output="./samples/$(basename $input .in).out"
   # if theres no matching output, just check the return is 0 and show ok
   if [ ! -f "$output" ]; then
     if $1 < $input > /dev/null; then
       echo -en $ok;
     else
       echo -en $xx;
+      all_ok=false;
     fi
-    echo ": sample '"$(basename input)"'";
+    echo ": sample '"$(basename $input)"'";
   else
     dc=$("$1" < $input | diff -U 0 $output - | grep ^@ | wc -l)
     if [ "$dc" -eq "0" ]; then
-      echo -e $ok": sample '"$(basename input)"'";
+      echo -e $ok": sample '"$(basename $input)"'";
     else
-      echo -e $xx": sample '"$(basename input)"'";
+      echo -e $xx": sample '"$(basename $input)"'";
       echo -e "\t$dc mismatched lines";
+      all_ok=false;
     fi
   fi
 done
--- a2/watch-test.sh
+++ a2/watch-test.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 # TODO: time runs
-find src/ | entr -cns "make run input=samples/$1.in | diff -y samples/$1.out -"
+find src/ | entr -cns "make run -s input=samples/$1.in | diff samples/$1.out -"
