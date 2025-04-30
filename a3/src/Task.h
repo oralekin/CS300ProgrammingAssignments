73782512f2d12d9eaebb33c77b3f02734106e6f1
Ekin Oral <oralekin@sabanciuniv.edu>
2025-04-30 12:16:16 +0300

    a3: comparison operators for Task

--- a3/src/Task.h
+++ a3/src/Task.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <ostream>
 
 class Task {
 	public:
@@ -7,5 +8,24 @@ class Task {
 	int priority;
 	std::string location;
 	std::string category;
-	Task(int id, int prio, std::string loc = "", std::string cat = "");
+
+	Task(int id, int prio, std::string loc = "", std::string cat = "") :
+			task_id(id), priority(prio), location(loc), category(cat) {}
+
+	// comparison operators should compare priority
+	inline bool operator<(const Task &rhs) const {
+		return priority < rhs.priority;
+	}
+
+	inline bool operator>(const Task &rhs) const {
+		return rhs < (*this);
+	}
+
+	inline bool operator>=(const Task &rhs) const {
+		return !((*this) < rhs);
+	}
+
+	inline bool operator<=(const Task &rhs) const {
+		return !(rhs < (*this));
+	}
 };
