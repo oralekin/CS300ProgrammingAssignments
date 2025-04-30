295c65aad5d95dd9420496fcdd0a76c96eab33c8
Ekin Oral <oralekin@sabanciuniv.edu>
2025-04-30 12:17:16 +0300

    update a3/test_all.sh i never learned to write shell scripts

--- a3/test-all.sh
+++ a3/test-all.sh
@@ -4,27 +4,32 @@ xx=$(tput setaf 1)$(tput bold)"XX"$(tput sgr0)
 ok=$(tput setaf 2)$(tput bold)"OK"$(tput sgr0)
 all_ok=true;
 
+if [ ! -x "$1" ]; then
+  echo -e $xx" no executable";
+  exit;
+fi
+
 if ! make $1; then
   echo -e $xx" failed to build";
   exit;
 fi
 
 for input in ./samples/*.in; do
-  output="$(basename input).out"
+  output="./samples/$(basename $input .in).out"
   # if theres no matching output, just check the return is 0 and show ok
   if [ ! -f "$output" ]; then
-    if $1 < $input > /dev/null; then
+    if $("$1" < $input > /dev/null); then
       echo -en $ok;
     else
       echo -en $xx;
     fi
-    echo ": sample '"$(basename input)"'";
+    echo ": sample '"$(basename $input)"'";
   else
     dc=$("$1" < $input | diff -U 0 $output - | grep ^@ | wc -l)
     if [ "$dc" -eq "0" ]; then
-      echo -e $ok": sample '"$(basename input)"'";
+      echo -e $ok": sample '"$(basename $input)"'";
     else
-      echo -e $xx": sample '"$(basename input)"'";
+      echo -e $xx": sample '"$(basename $input)"'";
       echo -e "\t$dc mismatched lines";
     fi
   fi
